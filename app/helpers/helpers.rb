c0102af017f5b4c84812f15265285ef454be7b9e
nathan hains <nathanhains14@yahoo.com>
2021-03-18 13:52:57 -0400

    commit tree 604b4992984141b5af9eacdc9602fef98b749474

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+    def self.current_user(session_hash)
+        @user = User.find_by_id(session_hash[:user_id])
+    end
+
+    def self.is_logged_in?(session_hash)
+       if User.find_by_id(session_hash[:user_id])
+        true
+       else
+        false
+       end
+    end
 end
